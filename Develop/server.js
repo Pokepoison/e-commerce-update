b3f26a2df1e2e7ced3b152e1beadc45d6375fef4
Pokepoison <jordyaw7@gmail.com>
2023-09-11 16:50:03 -0400

    adds api routes and updates code

--- Develop/server.js
+++ Develop/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -14,3 +21,5 @@ app.use(routes);
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
+
+//Add code in server.js to start the Express server?
